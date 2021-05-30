4df1df23dc4fc1b582318a5a80ca7630033e0c82
Neragin <nmathirajan349@student.fuhsd.org>
2021-05-29 22:06:00 -0700

    Added PingCommand

---
+++ src/commands/public/PingCommand.ts
@@ -0,0 +1,21 @@
+import {Command} from "discord-akairo";
+import {Message} from "discord.js";
+
+export default class PingCommand extends Command {
+    public constructor() {
+        super("ping", {
+            aliases: ["ping"],
+            category: "Public Commands",
+            description: {
+                content: "Check latency between the bot and the Discord API",
+                usage: "ping",
+                examples: ["ping"]
+            },
+            ratelimit: 3,
+            });
+    }
+    public async exec(message: Message): Promise<Message> {
+        let initial = await message.util.reply()
+        return message.util.send(`ping: \`${this.client.ws.ping}ms\``);
+    }
+}

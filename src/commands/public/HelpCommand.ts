e800c1671b9f89dcbe9dd038dcc7c830b61ba65b
Neragin <nmathirajan349@student.fuhsd.org>
2021-05-30 13:31:59 -0700

    Added Help Command

---
+++ src/commands/public/HelpCommand.ts
@@ -0,0 +1,60 @@
+import {Command} from "discord-akairo";
+import {Message, MessageEmbed} from "discord.js";
+import {stripIndents} from "common-tags";
+
+export default class HelpCommand extends Command {
+    public constructor() {
+        super("help", {
+            aliases: ["help", "commands"],
+            category: "Public Commands",
+            description: {
+                content: "view avaliable commands",
+                usage: "help [command]",
+                examples: [
+                    "help",
+                    "help ping",
+                ]
+            },
+            ratelimit: 3,
+            args: [{
+                id: "command",
+                type: "commandAlias",
+                default: null,
+            }]
+        });
+    }
+    public async exec(message: Message, {command}: {command: Command}): Promise<Message> {
+        if (command) {
+            return message.channel.send(new MessageEmbed()
+                .setAuthor(`Help | ${command}`, this.client.user.displayAvatarURL())
+                .setColor("RANDOM")
+                .setDescription(stripIndents`
+                **Description:**
+                ${command.description.content || "No content provided."}
+                
+                **Usage:**
+                ${command.description.usage || "No usage provided."}
+                
+                **Examples:**
+                ${command.description.examples ? command.description.examples.map(e =>`\`${e}\``).join("\n"): "No Example Provided"}
+                `)
+
+            )
+        }
+        const embed = new MessageEmbed()
+            .setAuthor(`Help | ${this.client.user.username}`, this.client.user.displayAvatarURL())
+            .setColor("RANDOM")
+            .setFooter(`${this.client.commandHandler.prefix} help [command] for more information for a command`);
+        for(const category of this.handler.categories.values()) {
+            if (["default"].includes(category.id)) continue;
+
+            embed.addField(category.id, category
+                .filter(cmd => cmd.aliases.length > 0)
+                .map(cmd => `**\`${cmd}\`**`)
+                .join(", ") || "No commands in this category,"
+            );
+
+        }
+        return message.channel.send(embed);
+    }
+}

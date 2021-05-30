8821bae0fa76e3deb539f77c58e36c1cdf55f648
Neragin <nmathirajan349@student.fuhsd.org>
2021-05-29 18:30:22 -0700

    initial commit + Basic Bot

---
+++ src/bot.ts
@@ -0,0 +1,5 @@
+import {token, owners} from "./config";
+import BotClient from "./client/BotClient"
+
+const client: BotClient = new BotClient({token, owners});
+client.start();
---
+++ src/client/BotClient.ts
@@ -0,0 +1,70 @@
+import {AkairoClient, CommandHandler, ListenerHandler} from "discord-akairo";
+import {User, Message} from "discord.js";
+import {join} from "path";
+import {prefix, owners} from "../config";
+
+declare module "discord-akairo" {
+
+    interface AkairoClient {
+        commandHandler: CommandHandler;
+        listenerHandler: ListenerHandler;
+    }
+}
+
+interface BotOptions {
+    token?: string;
+    owners?: string;
+}
+
+export default class BotClient extends AkairoClient{
+    public config: BotOptions;
+    public listenerHandler: ListenerHandler = new ListenerHandler(this, {
+        directory: join(__dirname, "..", "listeners")
+    })
+    public commandHandler: CommandHandler = new CommandHandler(this, {
+        directory: join( __dirname, "..", "commands"),
+        prefix: prefix,
+        allowMention: true,
+        handleEdits: true,
+        commandUtil: true,
+        commandUtilLifetime: 3e5,
+        defaultCooldown: 3e4,
+        argumentDefaults: {
+            prompt: {
+                modifyStart: (_: Message, str): string => `${str}\n\nType \`cancel\` to cancel the command`,
+                modifyRetry: (_: Message, str): string => `${str}\n\nType \`cancel\` to cancel the command`,
+                timeout: "You took too long, and the command has been cancelled",
+                ended: "You exceeded the maximum amount of tries, so the command has been cancelled",
+                cancel: "This command has been cancelled",
+                retries: 3,
+                time: 3e4,
+            },
+            otherwise: "",
+        },
+        ignorePermissions: owners,
+    });
+
+    public constructor(config: BotOptions) {
+        super({ ownerID: config.owners,
+            });
+
+        this.config = config;
+    }
+
+    private async _init(): Promise<void> {
+        this.commandHandler.useListenerHandler(this.listenerHandler);
+        this.listenerHandler.setEmitters({
+            commandHandler: this.commandHandler,
+            listenerHandler: this.listenerHandler,
+            process,
+            });
+
+        this.commandHandler.loadAll();
+        this.listenerHandler.loadAll();
+    }
+
+    public async start(): Promise<string> {
+        await  this._init();
+        return this.login(this.config.token);
+    }
+}

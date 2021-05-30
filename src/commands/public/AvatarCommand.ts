c1777f83fba18ac9ca7ba2f3482db126e0a89690
Neragin <nmathirajan349@student.fuhsd.org>
2021-05-29 23:39:24 -0700

    improved AvatarCommand.ts

--- src/commands/public/AvatarCommand.ts
+++ src/commands/public/AvatarCommand.ts
@@ -1,10 +1,10 @@
 import {Command} from "discord-akairo";
-import {Message, GuildMember, MessageEmbed, ImageSize} from "discord.js";
+import {GuildMember, ImageSize, Message, MessageEmbed} from "discord.js";
 
 export default class AvatarCommand extends Command {
     public constructor() {
         super("avatar", {
-            aliases: ["avatar","av"],
+            aliases: ["avatar", "av"],
             category: "Public Commands",
             description: {
                 content: "Displays the avatar of the member",
@@ -12,9 +12,10 @@ export default class AvatarCommand extends Command {
                 examples: [
                     "avatar",
                     "avatar @user#0001",
+                    "avatar user",
                 ]
             },
-            ratelimit:3,
+            ratelimit: 3,
             args: [
                 {
                     id: "member",
@@ -24,7 +25,7 @@ export default class AvatarCommand extends Command {
                 },
                 {
                     id: "size",
-                    type: (_:Message, str:string): null | number => {
+                    type: (_: Message, str: string): null | number => {
                         return str && !isNaN(Number(str)) && [16, 32, 64, 128, 256, 512, 1024, 2048].includes(Number(str)) ? Number(str) : null;
                     },
                     match: "option",
@@ -34,11 +35,12 @@ export default class AvatarCommand extends Command {
             ]
         });
     }
-    public exec(message: Message, {member, size}: {member: GuildMember, size: number}): Promise<Message> {
+
+    public exec(message: Message, {member, size}: { member: GuildMember, size: number }): Promise<Message> {
         return message.util.send(new MessageEmbed()
             .setTitle("avatar")
             .setColor("RANDOM")
-            .setImage(member.user.displayAvatarURL({ size: size as ImageSize}))
+            .setImage(member.user.displayAvatarURL({size: size as ImageSize}))
         );
     }
 }

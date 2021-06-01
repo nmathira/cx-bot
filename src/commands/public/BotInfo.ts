89f9bb4fbfd1bc5ff3f3829f683b1a24f063a1c8
Neragin <nmathirajan349@student.fuhsd.org>
2021-06-01 13:33:32 -0700

    Added botinfo command

---
+++ src/commands/public/BotInfo.ts
@@ -0,0 +1,26 @@
+import {Command} from "discord-akairo";
+import {Message, MessageEmbed} from "discord.js";
+import {version} from "../../config"
+
+export default class BotInfo extends Command {
+    public constructor() {
+        super("botinfo", {
+            aliases: ["botinfo"],
+            category: "Public Commands",
+            description: {
+                content: "Displays general info about the bot",
+                usage: "botinfo",
+                examples: ["botinfo"]
+            },
+        });
+    }
+    public async exec(message: Message, ): Promise<Message>{
+        return message.util.send(new MessageEmbed()
+            .setTitle("Invite Link")
+            .setURL(await this.client.generateInvite({permissions: "ADMINISTRATOR"}))
+            .addFields({name: "version", value: version})
+            .setAuthor(this.client.user.tag, this.client.user.displayAvatarURL())
+            .setTimestamp(Date.now())
+        )
+    }
+}

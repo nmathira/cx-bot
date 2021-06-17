86d5def0ec9bdd637e113077ff3bae325bf55cbb
Neragin <niranjan@nmathirajan.xyz>
2021-06-17 13:25:49 -0700

    added cog command

---
+++ src/bot/commands/admin/Cog.ts
@@ -0,0 +1,28 @@
+import {Command} from "discord-akairo";
+import { Message} from "discord.js";
+
+export default class Ping extends Command {
+    public constructor() {
+        super("cog", {
+            aliases: ["cog", "coggers"],
+            category: "Owner",
+            ownerOnly: true,
+            args: [{
+                id: "action",
+                type: "string",
+            },
+                {
+                    id: "command",
+                    type: "command"
+
+                }],
+            ratelimit: 3,
+        });
+    }
+
+    public async exec(message: Message, args: any): Promise<Command | undefined> {
+        if (args.action === "reload") return this.client.commandHandler.reload(args.command);
+        else if (args.action === "unload") return this.client.commandHandler.remove(args.command);
+        else await message.util!.send("that command doesn't exist or isn't loaded");
+    }
+}

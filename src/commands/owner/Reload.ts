92f73ac008855aa8429f0cffb7d726b7d3bc02f1
Neragin <niranjan@nmathirajan.xyz>
2021-07-19 16:43:15 -0700

    Google Sheets API stuff

--- src/commands/owner/Reload.ts
+++ src/commands/owner/Reload.ts
@@ -1,5 +1,5 @@
-import {Command} from "discord-akairo";
-import {Message} from "discord.js";
+import { Command } from "discord-akairo";
+import { Message } from "discord.js";
 
 
 export default class LoadCommand extends Command {

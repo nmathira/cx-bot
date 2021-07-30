4eab27b1f7eafd0fa1bc20b615a2cb728a0db234
Neragin <niranjan@nmathirajan.xyz>
2021-07-29 23:13:44 -0700

    added eslint (might add prettier in the future IDK)

--- config/config.example.ts
+++
@@ -1,5 +0,0 @@
-export let token = "token here";
-export let prefix = "prefix here";
-export let owners = "owner id here";
-export let version = "version number here";
-export let hypixelapi = "hypixel api key here";

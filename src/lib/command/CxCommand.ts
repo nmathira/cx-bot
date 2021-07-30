f503a727ec67262af1b808a498dbbbcadcde6b9e
Neragin <niranjan@nmathirajan.xyz>
2021-07-30 15:37:41 -0700

    Added ping command

---
+++ src/lib/command/CxCommand.ts
@@ -0,0 +1,29 @@
+import { Command, CommandOptions, PieceContext } from "@sapphire/framework";
+
+export interface CxCommandOptions extends CommandOptions {
+  category?: string,
+  examples?: string[] | string,
+  usage?: string,
+}
+
+export abstract class CxCommand extends Command {
+  public category: Readonly<string>;
+  public examples: Readonly<string[] | string>;
+  public usage: Readonly<string>;
+
+  protected constructor(context: PieceContext, options: CxCommandOptions) {
+    super(context, options);
+    this.category = options.category ?? "";
+    this.examples = options.examples ?? "";
+    this.usage = options.usage ?? "";
+  }
+
+  public toJSON(): Record<string, any> {
+    return {
+      ...super.toJSON(),
+      content: this.category,
+      examples: this.examples,
+      usage: this.usage,
+    };
+  }
+}

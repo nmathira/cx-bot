9722b9be674fb8d944411df48a33ed5e77a09a38
Neragin <niranjan@nmathirajan.xyz>
2021-08-06 20:04:39 -0700

    Piece Commands

--- src/bot/commands/Owner/loadall.ts
+++ src/bot/commands/Owner/loadall.ts
@@ -2,7 +2,6 @@ import type { Message } from "discord.js";
 import { ApplyOptions } from "@sapphire/decorators";
 import type { CxCommandOptions } from "@typings/index";
 import { CxCommand } from "@lib/extensions/CxCommand";
-import type { Args } from "@sapphire/framework";
 
 @ApplyOptions<CxCommandOptions>({
   name: "reload",
@@ -16,14 +15,8 @@ import type { Args } from "@sapphire/framework";
     "Sends the ping of CxBot's connection to Discord, as well as the ping from Discord.",
 })
 export class Reload extends CxCommand {
-  async run(message: Message, args: Args): Promise<Message> {
-    if (args.finished) {
-      this.container.stores.get("commands").forEach((cmd) => cmd.reload());
-      return message.channel.send("All commands reloaded!");
-    } else {
-      const command = await args.pick("string");
-      await this.container.stores.get("commands").get(command).reload();
-      return message.channel.send("Reloaded command: " + command);
-    }
+  async run(message: Message): Promise<Message> {
+    await this.container.stores.get("commands").loadAll();
+    return await message.channel.send("All commands loaded!");
   }
 }

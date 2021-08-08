f6578085369235d57cbaa7958a6eeded219b5129
Neragin <niranjan@nmathirajan.xyz>
2021-08-08 12:02:18 -0700

    Added compile command to compile typescript to javascript

---
+++ src/bot/commands/Owner/compile.ts
@@ -0,0 +1,31 @@
+import type { Message } from "discord.js";
+import { MessageEmbed } from "discord.js";
+import { ApplyOptions } from "@sapphire/decorators";
+import type { CxCommandOptions } from "@typings/index";
+import CxCommand from "@lib/extensions/CxCommand";
+import { safeExec } from "@lib/utils/util";
+import { codeBlock } from "@sapphire/utilities";
+
+@ApplyOptions<CxCommandOptions>({
+  aliases: ["tsc"],
+  category: "Owner",
+  usage: "cx compile",
+  examples: ["cx compile"],
+  preconditions: ["OwnerOnly"],
+  description: "compiles cxbot",
+  detailedDescription: "Compiles CxBot's typescript code into runnable code.",
+})
+export class Compile extends CxCommand {
+  async run(message: Message): Promise<Message> {
+    const { stderr } = await safeExec("tsc");
+    console.log(stderr);
+    const embed = new MessageEmbed()
+      .setTitle(stderr === "" ? "Compilation succeded!" : "Compilation Failed!")
+      .setDescription(
+        stderr === ""
+          ? "The core of CxBot has been backed up in memory, and will still function normally. However, if there were any breaking changes in the previous versions, make sure to restart me at your earliest convinience!"
+          : codeBlock("bash", stderr)
+      );
+    return message.channel.send({ embeds: [embed] });
+  }
+}

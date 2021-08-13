e78ce851a69d95803818eba70b3535610f0f44d4
Neragin <niranjan@nmathirajan.xyz>
2021-08-12 21:59:41 -0700

    reformat launcher

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,6 @@
 import "module-alias/register";
-import type { BitFieldResolvable, IntentsString } from "discord.js";
 import dotEnvExtended from "dotenv-extended";
 import CxClient from "@lib/extensions/CxClient";
 
 dotEnvExtended.load();
-const cxbot: CxClient = new CxClient({
-  prefix: process.env.PREFIX,
-  intents: <BitFieldResolvable<IntentsString, number>>[
-    "DIRECT_MESSAGES",
-    "GUILDS",
-    "GUILD_MEMBERS",
-    "GUILD_BANS",
-    "GUILD_INVITES",
-    "GUILD_MESSAGES",
-    "GUILD_MESSAGE_REACTIONS",
-  ],
-});
-cxbot
-  .login(process.env.DISCORD_TOKEN)
-  .then(() => cxbot.logger.info("we logged in baby"))
-  .catch(() => {
-    cxbot.logger.fatal("oh no, bot doesn't have life support");
-    cxbot.destroy();
-    process.exit(1);
-  });
+new CxClient().start();
--- src/lib/extensions/CxClient.ts
+++ src/lib/extensions/CxClient.ts
@@ -1,37 +1,43 @@
-import type { BitFieldResolvable, IntentsString } from "discord.js";
-import type { SapphirePrefix } from "@sapphire/framework";
 import { SapphireClient } from "@sapphire/framework";
 import { TaskStore } from "@lib/Task/TaskStore";
 import { join } from "path";
 
 export default class CxClient extends SapphireClient {
-  public constructor({
-    prefix = process.env.PREFIX,
-    intents = [
-      "DIRECT_MESSAGES",
-      "GUILDS",
-      "GUILD_MEMBERS",
-      "GUILD_BANS",
-      "GUILD_INVITES",
-      "GUILD_MESSAGES",
-      "GUILD_MESSAGE_REACTIONS",
-    ],
-  }: {
-    prefix: SapphirePrefix;
-    intents: BitFieldResolvable<IntentsString, number>;
-  }) {
+  public constructor() {
     super({
       baseUserDirectory: join(__dirname, "..", "..", "bot"),
       caseInsensitiveCommands: true,
       caseInsensitivePrefixes: true,
-      defaultPrefix: prefix,
-      intents: intents,
+      defaultPrefix: process.env.PREFIX,
+      intents: [
+        "GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_EMOJIS_AND_STICKERS",
+        "GUILD_VOICE_STATES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+      ],
     });
     // Store.defaultStrategy.onLoad = (store, piece) => container.logger.info(`Loading ${store.name}:${piece.name}`);
     this.stores.register(new TaskStore());
   }
 
   public fetchPrefix = (): string => process.env.PREFIX;
+
+  // public start(): SapphireClient {
+  public start(): void {
+    this.login(process.env.DISCORD_TOKEN)
+      .then(() => this.logger.info("Logged in as " + this.user.username))
+      .catch(() => {
+        this.logger.fatal("bot has fucking died");
+        this.destroy();
+        process.exit(1);
+      });
+    // return this;
+  }
 }
 declare module "@sapphire/pieces" {
   interface StoreRegistryEntries {

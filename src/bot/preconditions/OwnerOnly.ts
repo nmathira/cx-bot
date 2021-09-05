26ceb6d22dc34b39050b6354ea2a42199b0358e2
Niranjan M <niranjan@nmathirajan.xyz>
2021-09-05 11:19:20 -0700

    Featureful (#4)
    * testing linter

--- src/bot/preconditions/OwnerOnly.ts
+++ src/bot/preconditions/OwnerOnly.ts
@@ -6,6 +6,6 @@ export default class OwnerOnly extends Precondition {
   public run(message: Message): PreconditionResult {
     return process.env.OWNER === message.author.id
       ? this.ok()
-      : this.error({ message: "This message can only be used by my owner." });
+      : this.error({ message: "This message can only be used by the owner of me." });
   }
 }

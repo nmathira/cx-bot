bc9fc5b1219b7f9d1cccda5d91f07029504f0e82
Neragin <niranjan@nmathirajan.xyz>
2021-11-14 22:06:34 -0800

    funny joke

---
+++ src/bot/listeners/messageCreate.ts
@@ -0,0 +1,16 @@
+import type { PieceContext } from "@sapphire/framework";
+import { container, Listener } from "@sapphire/framework";
+import { Message } from "discord.js";
+
+export default class onMessage extends Listener {
+  public constructor(context: PieceContext) {
+    super(context);
+  }
+
+  public run(message: Message): Promise<Message> {
+    if (message.guildId = "893383816402575401") {
+      if (message.content.includes("fr")) return message.channel.send("ong")
+      if (message.content.includes("ong") && !message.author.bot) return message.channel.send("no cap")
+    }
+  }
+}
